482de37ba5a416ba088c58fbbcb7d75144469746
Andrei <Bananer95@mail.ru>
2025-07-19 14:40:16 +0200

    fix: finally fixed deploy

--- rs-react-app/vite.config.ts
+++ rs-react-app/vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/bananer95-REACT2025Q3/',
+  base: '/rs-react-app/',
   plugins: [react(), tailwindcss()],
 });
